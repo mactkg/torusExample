4972657828514b759d6aa9fc4b036bc9383a1571
mactkg <mactkg@gmail.com>
2018-07-24 09:42:51 +0900

    add failed pattern

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,77 +2,23 @@
 #include "GLFW/glfw3.h"
 
 //--------------------------------------------------------------
+// First Proto: experimentation of glfwSetInputMode
+//--------------------------------------------------------------
 void ofApp::setup(){
     auto glfw = ((ofAppGLFWWindow*)ofGetWindowPtr())->getGLFWWindow();
     glfwSetInputMode(glfw,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
+    
 }
 
-//--------------------------------------------------------------
 void ofApp::update(){
     ofLogNotice() << ofGetMouseX() << ", " << ofGetMouseY();
 }
 
-//--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
     auto x = calcTorus(ofGetMouseX(), ofGetWidth());
     auto y = calcTorus(ofGetMouseY(), ofGetHeight());
     
     ofDrawCircle(x, y, 50);
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
 }
 
 int ofApp::calcTorus(int n, int m) {
@@ -81,4 +27,81 @@ int ofApp::calcTorus(int n, int m) {
     } else {
         return m - (-n % m);
     }
+}
+
+//--------------------------------------------------------------
+// Second Proto: implement torus cursor system using
+//               ofGetMouseX/ofGetPreviousMouseX
+//--------------------------------------------------------------
+void ofApp2::setup(){
+    auto glfw = ((ofAppGLFWWindow*)ofGetWindowPtr())->getGLFWWindow();
+    glfwSetInputMode(glfw,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
+    
+    p = ofPoint(ofGetWidth()/2, ofGetHeight()/2);
+    pp = ofPoint(ofGetWidth()/2, ofGetHeight()/2);
+}
+
+void ofApp2::update(){
+    p.x = calcTorus(p.x + ofGetMouseX() - ofGetPreviousMouseX(), ofGetWidth());
+    p.y = calcTorus(p.y + ofGetMouseY() - ofGetPreviousMouseY(), ofGetHeight());
+}
+
+void ofApp2::draw() {
+    ofDrawCircle(p, 50);
+    debugDraw();
+}
+
+int ofApp2::calcTorus(int n, int m) {
+    if (n >= 0) {
+        return n % m;
+    } else {
+        return m - (-n % m);
+    }
+}
+
+//--------------------------------------------------------------
+// Third Proto: implement torus cursor system using
+//               hand-made ofGetPreviousMouseX
+//--------------------------------------------------------------
+void ofApp3::setup(){
+    auto glfw = ((ofAppGLFWWindow*)ofGetWindowPtr())->getGLFWWindow();
+    glfwSetInputMode(glfw,GLFW_CURSOR,GLFW_CURSOR_DISABLED);
+    
+    p = ofPoint(ofGetWidth()/2, ofGetHeight()/2);
+    pp = ofPoint(ofGetWidth()/2, ofGetHeight()/2);
+}
+
+void ofApp3::update(){
+    p.x = calcTorus(p.x + ofGetMouseX() - pp.x, ofGetWidth());
+    p.y = calcTorus(p.y + ofGetMouseY() - pp.y, ofGetHeight());
+    pp.set(ofGetMouseX(), ofGetMouseY());
+}
+
+void ofApp3::draw() {
+    ofDrawCircle(p, 50);
+}
+
+int ofApp3::calcTorus(int n, int m) {
+    if (n >= 0) {
+        return n % m;
+    } else {
+        return m - (-n % m);
+    }
+}
+
+void debugDraw() {
+    ofPushStyle();
+    
+    ofSetColor(255, 0, 0);
+    ofDrawLine(ofGetMouseX(), 0, ofGetMouseX(), ofGetHeight());
+    ofDrawLine(0, ofGetMouseY(), ofGetWidth(), ofGetMouseY());
+    
+    ofSetColor(0, 255, 0);
+    ofDrawLine(ofGetPreviousMouseX(), 0, ofGetPreviousMouseX(), ofGetWidth());
+    ofDrawLine(0, ofGetPreviousMouseY(), ofGetWidth(), ofGetPreviousMouseY());
+    
+    ofSetColor(0, 0, 255);
+    ofDrawLine(ofGetMouseX(), ofGetMouseY(), ofGetPreviousMouseX(), ofGetPreviousMouseY());
+    
+    ofPopStyle();
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -9,17 +9,31 @@ class ofApp : public ofBaseApp{
 		void update();
 		void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
         int calcTorus(int n, int m);
 };
+
+class ofApp2 : public ofBaseApp{
+    
+    public:
+        void setup();
+        void update();
+        void draw();
+    
+        int calcTorus(int n, int m);
+
+    ofPoint p, pp;
+};
+
+class ofApp3 : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    int calcTorus(int n, int m);
+    
+    ofPoint p, pp;
+};
+
+void debugDraw();
